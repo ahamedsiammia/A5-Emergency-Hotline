0ae418f47fb3338e17eca87264e5833d2da9e97f
ahamedsiammia <md7559263@gmail.com>
2025-08-29 17:36:28 +0600

    added copy button functionality

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ cardContainerHerat.addEventListener('click',function(e){
     } 
 })
 
-
 // clear button functionality
 document.getElementById('clear-btn').addEventListener('click',function(e){
     e.preventDefault()
@@ -25,6 +24,30 @@ document.getElementById('clear-btn').addEventListener('click',function(e){
       
 })
 
+// call button click and add history
+//  document.getElementById('call-btn').addEventListener('click',function(e){
+//     e.preventDefault()
+//     console.log('clicked call button')
+//     const allCallHistory = document.getElementById('all-call-history')
+//     console.log(allCallHistory)
+//     const parentCallHistory = document.getElementById('call-history').innerText
+//     console.log(parentCallHistory)
+//     const creatElement = document.createElement('div')
+//     creatElement = allCallHistory
+//     parentCallHistory.appendChild(allCallHistory);
+//  })
+// copy button functionality
 
 
+cardContainerHerat.addEventListener('click',function(e){
+    e.preventDefault()
+    if(e.target.closest('.copy-btn')){
 
+        const cardContainerHerat = parseInt(1)
+        
+        const navberCopyBtn = parseInt(document.getElementById('navber-copy-btn').innerText)
+
+        const addCopy = navberCopyBtn + cardContainerHerat
+        document.getElementById('navber-copy-btn').innerText = addCopy
+    }
+})
