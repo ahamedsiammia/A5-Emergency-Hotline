1281df65bc55c88cba4e4ae6e5eb748f28ba5c23
ahamedsiammia <md7559263@gmail.com>
2025-08-29 12:15:18 +0600

    added call history

---
+++ index.js
@@ -0,0 +1,22 @@
+// heart functonality
+
+const cardContainerHerat = document.getElementById('card-container-heart');
+
+cardContainerHerat.addEventListener('click',function(e){
+    e.preventDefault()
+    if (e.target.closest('.card-heart')) {
+    
+    const cardContainerHerat = parseInt(1)
+    
+    const nevberHeart = parseInt(document.getElementById('nevber-heart').innerText);
+    
+
+    const addHeart = cardContainerHerat + nevberHeart
+    document.getElementById('nevber-heart').innerText = addHeart
+    } 
+})
+
+
+
+
+
